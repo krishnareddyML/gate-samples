4933895a2dc0c190bd1f09ddd842c4bf46fbb02b
krishnareddyML <krishnareddy.nandyala@gmail.com>
2019-03-15 17:29:58 +0530

    Commit

---
+++ sb-gate-multithread-console/src/main/java/gate/samples/SbGateMultithreadConsoleApplication.java
@@ -0,0 +1,13 @@
+package gate.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbGateMultithreadConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbGateMultithreadConsoleApplication.class, args);
+	}
+
+}
